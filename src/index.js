eabe1f8d4a1f7864bcd9e38cba26ee4ac296d80f
David Castrillo <[rambusrim@gmail.com]>
2022-09-19 12:11:23 +0200

    primer commit del proyecto

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import { saludar } from './js/component';
+import './styles.css';
+
+
+const nombre = 'David';
+saludar(nombre);
---
+++ src/js/component.js
@@ -0,0 +1,17 @@
+import '../css/component.css';
+// import webpacklogo from '../assets/img/webpack-logo.png';
+export const saludar = ( nombre ) => {
+
+    
+    console.log('creando etiqueta h1');
+    const h1 = document.createElement('h1');
+    h1.innerText = `Hola, ${ nombre}`;
+    document.body.append( h1);
+
+    // img
+    // const img = document.createElement('img')
+    // img.src = webpacklogo;
+    // document.body.append(img);
+    // console.log(webpacklogo);
+    
+}
